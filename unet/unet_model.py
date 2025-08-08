8716bd01bdffdf94834d296c6b7a12d4422ab1af
yashghadale <yashghadale511@gmail.com>
2025-08-08 13:59:13 +0530

    Update unet_model.py

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -1,26 +1,91 @@
-""" Full assembly of the parts to form the complete network """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.checkpoint import checkpoint
 
-from .unet_parts import *
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, use_checkpoint=False):
+        super().__init__()
+        self.use_checkpoint = use_checkpoint
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
 
+    def forward(self, x):
+        if self.use_checkpoint and self.training:
+            return checkpoint(self.conv, x)
+        else:
+            return self.conv(x)
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels, use_checkpoint=False):
+        super().__init__()
+        self.pool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels, use_checkpoint)
+        )
+
+    def forward(self, x):
+        return self.pool_conv(x)
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True, use_checkpoint=False):
+        super().__init__()
+        self.use_checkpoint = use_checkpoint
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, use_checkpoint)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels, use_checkpoint)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        if self.use_checkpoint and self.training:
+            return checkpoint(self.conv, x, use_reentrant=False)
+
+        else:
+            return self.conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
-        super(UNet, self).__init__()
+    def __init__(self, n_channels, n_classes, bilinear=True, use_checkpoint=False):
+        super().__init__()
+        self.use_checkpoint = use_checkpoint
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
+        self.inc = DoubleConv(n_channels, 64, use_checkpoint)
+        self.down1 = Down(64, 128, use_checkpoint)
+        self.down2 = Down(128, 256, use_checkpoint)
+        self.down3 = Down(256, 512, use_checkpoint)
         factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.down4 = Down(512, 1024 // factor, use_checkpoint)
+        self.up1 = Up(1024, 512 // factor, bilinear, use_checkpoint)
+        self.up2 = Up(512, 256 // factor, bilinear, use_checkpoint)
+        self.up3 = Up(256, 128 // factor, bilinear, use_checkpoint)
+        self.up4 = Up(128, 64, bilinear, use_checkpoint)
+        self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -35,14 +100,8 @@ class UNet(nn.Module):
         logits = self.outc(x)
         return logits
 
-    def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
+    def enable_checkpointing(self):
+        self.use_checkpoint = True
+        for module in self.modules():
+            if isinstance(module, (DoubleConv, Up, Down)):
+                module.use_checkpoint = True
